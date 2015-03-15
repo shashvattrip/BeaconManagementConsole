8efb57acf8a301768fa4b512bf521d294ccc7afc
shashvattrip <shashvattrip@gmail.com>
2015-03-15 20:21:37 +0530

    First commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('bobApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
